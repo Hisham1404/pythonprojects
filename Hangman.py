34ac3122ced46c0efecc9b9b7ffc6e1f94bd7cf2
Mohammed hisham <96807846+Hisham1404@users.noreply.github.com>
2023-04-24 07:08:52 +0530

    Add files via upload

---
+++ Hangman.py
@@ -0,0 +1,32 @@
+import random
+
+with open("same.txt",'r') as f:
+    words = f.readlines()
+word = random.choice(words)[:-1]
+
+allowed_errors = 7
+guesses = []
+done = False
+while not done:
+    for letter in word:
+        if letter.lower() in guesses:
+            print(letter, end=" ")
+        else:
+            print("_",end=" ")
+    print("")
+
+    guess = input(f"Allowed errors left {allowed_errors}, next guess: ")
+    guesses.append(guess.lower())
+    if guess.lower() not in word.lower():
+        allowed_errors -=1
+        if allowed_errors==0:
+            break
+
+    done = True
+    for letter in word:
+        if letter.lower() not in guesses:
+            done = False
+if done:
+    print(f"You found the word! the word is {word}")
+else:
+    print(f"Game over the word was {word}")
